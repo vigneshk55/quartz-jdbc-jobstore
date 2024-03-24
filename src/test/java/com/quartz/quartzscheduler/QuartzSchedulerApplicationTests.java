d70df4feda7726663cd3b09a1150cdfb9f4ff74d
vigneshk55 <vigneshk5510@gmail.com>
2024-03-24 14:30:30 +0530

    project initial commit

---
+++ src/test/java/com/quartz/quartzscheduler/QuartzSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartz.quartzscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
